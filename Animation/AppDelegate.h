1d93e79d97dfb94ae3237ae1c8997b6b1f954c9e
LiuChao <893081892@qq.com>
2017-08-04 17:56:32 +0800

    Initial Commit

---
+++ Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by 刘超 on 2017/8/4.
+//  Copyright © 2017年 ogemray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
